55562c7f26444ac2dcf0e6319c3a4d50c284d1ca
Johnathon Frocillo <john.frocillo@gmail.com>
2021-04-11 15:51:48 -0400

    Implemented the ability to get an email when you forget your password.

--- src/components/ForgotPassword.vue
+++ src/components/ForgotPassword.vue
@@ -2,18 +2,25 @@
   <div>
     <h1>Reset Your Password</h1>
     <input type="text" v-model="email" placeholder="Email" /><br /><br />
-    <button @click="resetPasswordClick()">Reset Password</button>
+    <button :disabled="noInput" @click="resetPasswordClick()">
+      Reset Password
+    </button>
     <div>{{ message }}</div>
   </div>
 </template>
 
 <script lang="ts">
+import firebase from "firebase";
 import { Component, Vue } from "vue-property-decorator";
 
 @Component
 export default class ForgotPassword extends Vue {
   private message = "";
   private email = "";
+
+  get noInput(): boolean {
+    return this.email.length === 0;
+  }
 
   showMessageAndDismiss(m: string) {
     this.message = m;
@@ -24,11 +31,27 @@ export default class ForgotPassword extends Vue {
     }, 3000);
   }
 
+  showMessage(m: string) {
+    this.message = m;
+    setTimeout(() => {
+      // Auto disappear after 5 seconds
+      this.message = "";
+    }, 3000);
+  }
+
   resetPasswordClick() {
     // call firebase thing to reset password. Plenty of docs from firebase website
-    this.showMessageAndDismiss(
-      "Check your email for a link to reset your password! (not done yet)"
-    );
+    var auth = firebase.auth();
+    auth
+      .sendPasswordResetEmail(this.email)
+      .then(() => {
+        this.showMessageAndDismiss(
+          "Check your email for a link to reset your password!"
+        );
+      })
+      .catch((err) => {
+        this.showMessage(err);
+      });
   }
 }
 </script>
